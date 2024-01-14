cd10c9d4c6a62f1772bc8aba42a86ba9100f8d54
Hangil-cho <120692405+Hangil-cho@users.noreply.github.com>
2024-01-15 01:24:27 +0900

    Update bert.py

--- BERT/bert.py
+++ BERT/bert.py
@@ -6,4 +6,15 @@ import torch
 
 from pytorch_transformers import BertTokenizer, BertForSequenceClassification, BertConfig
 
+device = torch.device('cuda')
 model = BertForSequenceClassification.from_pretrained('bert-base-multilingual-cased')
+model.to(device)
+model.train()
+
+# for epoch in range(100):
+#     #Dataset X, Y
+#     loss, logits = model(X, labels = Y) #X: 두개의 문장, Y: 두 문장이 연속인가? (1/0)
+#     pred = torch.argmax(F.softmax(logits), dim = 1)
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
